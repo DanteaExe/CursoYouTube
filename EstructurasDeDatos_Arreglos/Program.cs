b0670e6ee22b1afa3eefe0b43b73676ffa9e250e
Dante <126302786+DanteaExe@users.noreply.github.com>
2024-09-22 22:33:06 -0600

    arrays class

---
+++ EstructurasDeDatos_Arreglos/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+int[] array = [7, 2, 56, 4, 1215, 6];
+
+int sumOfArray = array[0] + array[1] + array[2] + array[3] + array[4] + array[5];
+
+if(sumOfArray is 21)
+{
+    WriteLine($" The sum of each element of the array is: {sumOfArray}");
+}
+else if(sumOfArray is not 21)
+{
+    WriteLine("The sum of each element of the array is not: 21");
+    WriteLine($"is: {sumOfArray}");
+}
