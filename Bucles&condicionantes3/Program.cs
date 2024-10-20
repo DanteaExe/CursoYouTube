fdb9583a96aa70e47a2a12d4552a5a604b585709
Dante <126302786+DanteaExe@users.noreply.github.com>
2024-10-19 23:40:51 -0600

    nuestro primer ciclo

---
+++ Bucles&condicionantes3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+// int counter = 0;
+
+// while (counter < 15)
+// {
+//     counter++;
+//     WriteLine($"Hola! {counter}");
+// }
+string [] names = ["Pedro", "Alberto", "Pepe", "Luis", "Alejandra"];
+int counter = 0;
+
+while (counter < 5)
+{
+    WriteLine(names[counter]);
+    counter++;
+}
